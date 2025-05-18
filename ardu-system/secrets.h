4602f7fb591cd5cb3cead5a1f13a1a61432f2e04
Soohyun Hwang <81736415+TalkingPotato15@users.noreply.github.com>
2025-05-18 19:37:44 +0900

    Update secrets.h

--- ardu-system/secrets.h
+++ ardu-system/secrets.h
@@ -1,52 +1,14 @@
 /*
-
-          WIFI SETUP
-
+    WIFI SETUP
 */
 
 #define WIFI_SSID     ""
 #define WIFI_PASSWORD ""
 
 /*
-
-          FIREBASE SETUP
-
-  ------------------------------------------------
-  IMPORTANT: Choose Firebase Initialization Method
-  ------------------------------------------------
-
-  1. ** Test Mode (No Authentication) **:
-
-     - Ensure Firebase rules are set to allow public access. Set the rules as follows:
-       {
-         "rules": {
-           ".read": "true",
-           ".write": "true"
-         }
-       }
-
-  2. ** Locked Mode (With Authentication) **:
-
-     - Obtain your Firebase Authentication Token:
-       1. Open your Firebase Console: https://console.firebase.google.com/
-       2. Navigate to your project.
-       3. Click on the gear icon next to "Project Overview" and select "Project settings".
-       4. Go to the "Service accounts" tab.
-       5. In the "Database secrets" section, click on "Show" to reveal your authentication token.
-
-     - Ensure Firebase rules require authentication. Set the rules as follows:
-       {
-         "rules": {
-           ".read": "auth != null",
-           ".write": "auth != null"
-         }
-       }
-
-  Note: Using authentication is recommended for production environments to secure your data.
+    MQTT BROKER SETUP
 */
 
-/* Test Mode (No Authentication) */
-//#define REFERENCE_URL ""
-
-/* Uncomment the following line for Locked Mode (With Authentication) */
-//#define AUTH_TOKEN ""
+#define BROKER_IP     ""
+#define TOPIC_ID      ""
+#define PORT_ADDR     1883
