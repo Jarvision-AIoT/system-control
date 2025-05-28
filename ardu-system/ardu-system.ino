07ab801fe4c51aa4e12865e739d05fed099e671d
Soohyun Hwang <81736415+TalkingPotato15@users.noreply.github.com>
2025-05-28 13:02:45 +0900

    Update ardu-system.ino

--- ardu-system/ardu-system.ino
+++ ardu-system/ardu-system.ino
@@ -5,13 +5,11 @@
   
   Before running this code, make sure to check the "secrets.h" file
   for important configuration details such as Wi-Fi credentials and 
-  Firebase settings.
+  MQTT Broker settings.
 
   The "secrets.h" file should include:
   - Your Wi-Fi SSID and Password
-  - Your MQTT Broker URL or IP address
-  - Your selected MQTT Broker Topic
-  - Port for your Broker (Usually 1883)
+  - MQTT Broker (mosquitto) IP, Topic, Port Address
 
   Ensure that "secrets.h" is properly configured and includes the correct
   information for your project. Failure to do so may result in connection
@@ -21,12 +19,24 @@
   this sketch.
 */
 
+/*
+  ---------------------------------
+      INFO: ArduinoJson library   
+  ---------------------------------
+
+  Download ArduinoJson library from the Library Manager:
+  https://www.arduino.cc/reference/en/libraries/arduinojson/
+
+  For guidance on serialization and deserialization, visit:
+  https://arduinojson.org/v7/assistant/
+*/
+
 #include <Arduino.h>
-#include <WiFiS3.h>                 // For Arduino UNO R4 WiFi
+#include "secrets.h"                // Contains WIFI_SSID and WIFI_PASSWORD
 #include <ArduinoMqttClient.h>
 #include "PinDefinitionsAndMore.h"  // IR pin definitions
 #include <IRremote.hpp>
-#include "secrets.h"                // Contains WIFI_SSID and WIFI_PASSWORD
+#include <WiFiS3.h>                 // For Arduino UNO R4 WiFi
 
 #define DECODE_NEC
 #define DECODE_DISTANCE_WIDTH
@@ -39,27 +49,18 @@ MqttClient mqttClient(wifiClient);
 
 const char broker[] = BROKER_IP;
 const char topic[]  = TOPIC_ID;
-int        port     = 1883;
+int        port     = PORT_ADDR;
 
 unsigned long previousMillis = 0;
 const long interval = 500;
 decode_results results;
-
-/*
-  Note: if your wifi connection has some issue, try using fixed IP, gateway, subnet, and dns using code below.
-  This method WORK but not recommended.
-  IPAddress local_ip(?, ?, ?, ?);     // IP address that is not likely to be used
-  IPAddress gateway(?, ?, ?, ?);      // gateway checked from PC
-  IPAddress subnet(?, ?, ?, ?);       // subnet checked from PC
-  IPAddress dns(?, ?, ?, ?);          // We used Google Public DNS when testing
-*/
 
 void setup()
 {
   Serial.begin(9600);
   delay(2000);
 
-  Serial.println("Setting static IP...");
+  //Serial.println("Setting static IP...");
   //WiFi.config(local_ip, dns, gateway, subnet);
   
   Serial.print("Connecting to WiFi SSID: ");
@@ -67,9 +68,11 @@ void setup()
 
   WiFi.begin(ssid, pass);
   delay(3000);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  //Serial.println(WiFi.localIP());
+  //Serial.println(WiFi.subnetMask());
+  //Serial.println(WiFi.gatewayIP());
+  //Serial.println(WiFi.dnsServerIP());
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print("-");
   }
@@ -77,25 +80,25 @@ void setup()
   Serial.println("\nWiFi connected!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+  Serial.println("==============================");
 
   #if defined(ARDUINO_UNOWIFIR4)
     digitalWrite(LED_BUILTIN, HIGH);
   #endif
 
-  Serial.println("\n===== MQTT Connection Start =====");
+  Serial.println("===== MQTT Connection Start =====");
   Serial.print("Connecting to broker: ");
   Serial.print(broker);
   Serial.print(":");
   Serial.println(port);
 
-  if (!mqttClient.connect(broker, port))
-  {
+  if (!mqttClient.connect(broker, port)) {
     Serial.print("[ERROR] MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
     while (1); // stop here
   }
 
-  Serial.println("MQTT broker connected!");
+  Serial.println("Connected to MQTT broker!");
   mqttClient.subscribe(topic);
   Serial.print("Subscribed to topic: ");
   Serial.println(topic);
@@ -155,11 +158,11 @@ void receive_ir_data()
 
 int IRsignal(String cmd)
 {
-  if (cmd == "{\"type\":\"fist\"}") { sAddress = 0x00; sCommand = 0x02; sRepeats = 3; }
-  else if (cmd == "{\"type\":\"open\"}") { sAddress = 0x00; sCommand = 0x03; sRepeats = 3; }
-  else if (cmd == "{\"type\":\"ok_sign\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 1; }
-  else if (cmd == "{\"type\":\"point\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 2; }
-  else if (cmd == "{\"type\":\"peace\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 3; }
+  if (cmd == "{\"type\": \"Close\"}") { sAddress = 0x00; sCommand = 0x02; sRepeats = 3; }            //무드등 끄기
+  else if (cmd == "{\"type\": \"Open\"}") { sAddress = 0x00; sCommand = 0x03; sRepeats = 3; }        //무드등 키기
+  else if (cmd == "{\"type\": \"OK\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 1; }
+  else if (cmd == "{\"type\":\"Pointer\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 2; }
+  else if (cmd == "{\"type\": \"Peace\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 3; }
   else if (cmd == "{\"type\":\"standby\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 4; }
   else if (cmd == "{\"type\":\"thumbs_up\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 5; }
   else if (cmd == "{\"type\":\"rock\"}") { sAddress = 0x0123; sCommand = 0x45; sRepeats = 6; }
@@ -172,6 +175,7 @@ String prevCmd = "temp";
 void loop()
 {
   receive_ir_data();
+  //mqttClient.poll();
 
   int messageSize = mqttClient.parseMessage();
   if (messageSize)
@@ -188,6 +192,10 @@ void loop()
     {
       command += (char)mqttClient.read();
     }
+
+    //mqttClient.beginMessage(topic);
+    //mqttClient.print(command);
+    //mqttClient.endMessage();
 
     Serial.print("Parsed command: ");
     Serial.println(command);
